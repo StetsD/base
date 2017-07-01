dbe1d4c0c89f7ce6f3da30748c9dad6abb8af3e7
Stets Dmitrii <chortstd@gmail.com>
2017-07-01 16:39:06 +0300

    immutability

--- sources/js/func-tr/purity-immutability.js
+++ sources/js/func-tr/purity-immutability.js
@@ -1,38 +1,36 @@
-//чистые ф-ии
-function toArray(arr){
-	return (
-		(Array.isArray(arr) && arr)
-		|| (!Array.isArray(arr) && arr.length && [].slice.call(arr))
-		|| (typeof arr === 'string' && arr.split(''))
-		|| (typeof arr === 'object' && [])
-	)
-}
+//иммутабельность
 function partial(fun, arg1){
 	return function(){
 		var args = args.concat(toArray(arguments));
 		return fun.apply(fun, args);
 	}
 }
-
-function take(array, n){
-	n = n || 1;
-	var length = array ? array.length : 0;
-	if(!length) return [];
-	return array.slice(0, n < 0 ? 0 : n);
+function range(length){
+	var index = -1,
+	result = Array(length);
+	while(++index < length) result[index] = index;
+	return result;
 }
 
-function random(min, max){
-	return Math.floor(Math.random() * ((max || 0) - (min || 0) + 1) + min);
+function skipTake(n, coll){
+	var ret = [],
+		sz = coll.length;
+
+	for(var index = 0; index < sz; index += n){
+		ret.push(coll[index]);
+	}
+
+	return ret
 }
 
-var rand = partial(random, 1);
+console.log(skipTake(3, range(20)));
 
-take(repeatedly(100, partial(rand, 10)), 5);
+//immutability and Recursions
+function summ(array){
+	var result = 0;
+	var sz = array.length;
 
-function randString(len){
-	var ascii = repeatedly(len, partial(rand, 26));
+	for(var i = 0; i < sz; i++) result += array[i];
 
-	return ascii.map(function(n){
-		return n.toString(36);
-	}).join('');
+	return result;
 }
