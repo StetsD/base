ed394b80a63ccc233071945fee1f8e9164a4dcca
Stets Dmitrii <chortstd@gmail.com>
2017-07-02 17:21:20 +0300

    es6-patterns

---
+++ sources/js/es6-patterns/11 - Proxy/index.js
@@ -0,0 +1,35 @@
+class Subject {
+    constructor() {
+    }
+
+    Request (){
+    }
+}
+
+class RealSubject extends Subject {
+    constructor() {
+        super()
+        facade.log('RealSubject created')
+    }
+
+    Request (){
+        facade.log('RealSubject handles request')
+    }
+}
+
+class Proxy extends Subject {
+    constructor() {
+        super()
+        facade.log('Proxy created')
+    }
+
+    Request (){
+        this.realSubject = new RealSubject();
+        this.realSubject.Request();
+    }
+}
+
+function init_Proxy() {
+    var proxy = new Proxy()
+    proxy.Request()
+}
