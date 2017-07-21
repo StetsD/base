a224e63af4d5aa589f69cbc41d0638a43f54c59c
StetsD <Dmitriy.Stets@adwatch.ru>
2017-07-21 16:47:17 +0300

    simple midle

--- sources/js/red-of.js
+++ sources/js/red-of.js
@@ -16,3 +16,32 @@ $(function(){
         // console.log(store.getState().reducerInput);
     });
 })
+
+
+
+let f1 = data => () => {
+	console.log(data);
+	data = data * 10;
+	console.log(data);
+	return data;
+}
+
+let f2 = data => () => {
+	console.log(data);
+	data = data + 5;
+	console.log(data);
+	return data;
+}
+
+function app(data, func){
+	let fa;
+	let mutData;
+	func.forEach(f => {
+		let newF = f(mutData ? mutData : data);
+
+		mutData = newF();
+
+	})
+}
+
+app(10, [f1, f2])
